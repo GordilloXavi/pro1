ce08407f2e592c356c798252385493bb810129cf
Xavier Gordillo Ramos <xavier.gordillo@c6s308pc31.fib.upc.es>
2018-11-14 11:53:12 +0100

    .

--- P7/X68753.cc
+++ P7/X68753.cc
@@ -2,49 +2,6 @@
 #include <vector>
 using namespace std;
 
-void moda(int n){
-    
-    vector<int> V(n);
-
-    for(int i = 0; i<n; ++i)cin >> V[i];
-
-    vector<int>rank;
-    vector<int>occs;
-
-    for(int i = 0; i<n; ++i){
-
-        bool found = false;
-
-        for(int j = 0; j<rank.size() and not found; ++j){
-
-            if(V[i] == rank[j]){
-                occs[j]++;
-                found = true;
-            } 
-        } 
-        if(not found){
-            rank.push_back(V[i]);
-            occs.push_back(1);
-        } 
-
-    } 
-
-    int max = 0;//highest number
-    int m = 0;//most occurences
-
-    for(int i = 0; i<occs.size(); ++i){
-
-        if(occs[i] >= m){
-            if(max < rank[i])max = rank[i];
-            m = occs[i];
-        } 
-
-        cout << max << endl;
-        
-
-    } 
-
-} 
 
 int main(){
 
@@ -52,8 +9,29 @@ int main(){
 
     while(cin >> n){
 
-        moda(n);
+        vector<int> nums(n);
+
+        for(int i = 0; i<n; ++i)cin >> nums[i];
+
+        int moda = 0, rep = 0;
+
+        for(int i = 0; i<n; ++i){
+
+        int r = 0;
+
+        for(int j = 0; j<n; ++j)r += (nums[i] == nums[j]);
+
+            if(r > rep or (r == rep and nums[i]> moda)){
+                  moda = nums[i];
+                  rep = r;
+
+            } 
+
+        } 
+
+        cout << moda << endl;
 
     } 
+
 
 } 
