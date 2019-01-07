74e3726394677e42596500844f32b0d6895616f1
Xavi Gordillo <xavigordillo@Xavis-MacBook-Pro.local>
2019-01-07 22:54:26 +0100

    .

--- consolidation/P12284.cc
+++ consolidation/P12284.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -7,12 +8,30 @@ struct Word{
     int reps;
 } ;
 
+bool func(Word& a, Word& b){
+    return ( (a.reps == b.reps and a.w<b.w )or (a.reps > b.reps ) );
+} 
 
 
 int main(){
     int n,k;
     while(cin >> n >> k){
         vector<string> words(n);
-         
+        for(int i = 0; i<n; ++i)cin >> words[i];
+       sort(words.begin(), words.end()); 
+        vector<Word> Wreps;
+       for(int i =0 ; i<n; ++i) {
+           if(i > 0 and words[i] == words[i-1]){
+               Wreps[Wreps.size()-1].reps++;
+           } else{
+               Word w; 
+               w.w = words[i];
+               w.reps = 1;
+               Wreps.push_back(w);
+           } 
+       } 
+       sort(Wreps.begin(), Wreps.end(), func);
+       for(int i =0 ;i<k;++i)cout << Wreps[i].w << endl;
+       cout << "----------" << endl;
     } 
 } 
